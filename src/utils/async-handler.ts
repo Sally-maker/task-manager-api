e236e9d7362988155b5f4064aeea2032419990c7
Sally-maker <gabrielgbbarbosa624@gmail.com>
2026-05-05 20:20:29 -0300

    fix: add CORS, rate limiting, AppError, graceful shutdown and DATABASE_URL guard

--- src/utils/async-handler.ts
+++ src/utils/async-handler.ts
@@ -1,6 +1,9 @@
-import { Request, Response, NextFunction, RequestHandler } from 'express'
+import { NextFunction, Request, RequestHandler, Response } from 'express'
 
-export const asyncHandler = (fn: Function): RequestHandler =>
-  (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch(next)
+export const asyncHandler =
+  <T extends Request = Request>(
+    fn: (req: T, res: Response, next: NextFunction) => Promise<unknown>,
+  ): RequestHandler =>
+  (req, res, next) => {
+    Promise.resolve(fn(req as T, res, next)).catch(next)
   }
