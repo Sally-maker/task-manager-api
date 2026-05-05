75412c5ffd7fd85fb0a76bb1bab940a30a6b22bd
Sally-maker <gabrielgbbarbosa624@gmail.com>
2026-05-05 13:55:28 -0300

    chore: initial project setup
    - Node.js + TypeScript (ESM) with tsx runtime
    - Prisma v7 with adapter-pg for PostgreSQL
    - Jest with experimental-vm-modules for ESM tests
    - Environment example and gitignore configured

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'node',
+  testMatch: ['**/tests/**/*.test.ts'],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          module: 'ESNext',
+          moduleResolution: 'bundler',
+        },
+      },
+    ],
+  },
+  clearMocks: true,
+}
+
